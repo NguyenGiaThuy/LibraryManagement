3eaec609c07f05d40eda1f88f09e6ba236ff9467
thuy <nguyengiathuy1998@gmail.com>
2022-03-22 22:54:36 +0700

    Initialize ServerSide

---
+++ ServerSide/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerSide
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webHost =>
+            {
+                webHost.UseStartup<Startup>();
+            });
+        }
+    }
+}
---
+++ ServerSide/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace ServerSide
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment webHostEnvironment)
+        {
+            if(webHostEnvironment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+            }
+
+            applicationBuilder.UseRouting();
+
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
