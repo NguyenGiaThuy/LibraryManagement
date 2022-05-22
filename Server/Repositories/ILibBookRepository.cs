8b6fb8412414701d9b7cb711259d9e6771b7e094
Gia Thuận <92902379+lokito01@users.noreply.github.com>
2022-05-22 14:11:10 +0700

    add Books components

--- Server/Repositories/ILibBookRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Server.Repositories
-{
-    public interface ILibBookRepository
-    {
-    }
-}
---
+++ Server/Repositories/ILibBooksRepository.cs
@@ -0,0 +1,17 @@
+﻿using Server.Models;
+
+namespace Server.Repositories
+{
+    public interface ILibBooksRepository
+    {
+        public Task<List<LibBook>> GetBooksAsync();
+        public Task<List<LibBook>> GetBooksByGenreAsync(int genre);
+        public Task<List<LibBook>> GetBooksByAuthorAsync(string author);
+        public Task<List<LibBook>> GetBooksByPublisherAsync(string publisher);
+        public Task<LibBook> GetBookByIsbnAsync(string isbn);
+        public Task<LibBook> GetBooksByTitleAsync(string title);
+        public Task<string> AddBookAsync(LibBook book);
+        public Task<string> UpdateBookAsync(string isbn, LibBook book);
+        public Task<string> RemoveBookAsync(string userId);
+    }
+}
