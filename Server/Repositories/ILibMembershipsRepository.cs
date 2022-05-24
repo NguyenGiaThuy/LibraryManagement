853ea46b514e8f3c5f0c933d3558e71b20ded8f1
namdoanviet <nambmt2001@gmail.com>
2022-05-24 21:44:40 +0700

    create membership

---
+++ Server/Repositories/ILibMembershipsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Server.Models;
+
+namespace Server.Repositories
+{
+    public interface ILibMembershipsRepository
+    {
+        public Task<List<LibMembership>> GetMembershipsAsync();
+        public Task<LibMembership> GetMembershipByIdAsync(string membershipId);
+        public Task<string> CreateMembershipAsync(LibMembership membership);
+        public Task<string> DisableMembershipAsync(string membershipId);
+        public Task<string> EnableMembershipAsync(string membershipId);
+        public Task<string> ExtendMembershipAsync(string membershipId);
+    }
+}
---
+++ Server/Repositories/LibMembershipsRepository.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Repositories
+{
+    public class LibMembershipsRepository : ILibMembershipsRepository
+    {
+        private readonly LibraryManagementContext _context;
+
+        public LibMembershipsRepository(LibraryManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<LibMembership>> GetMembershipsAsync()
+        {
+            var result = await _context.LibMemberships.ToListAsync();
+            return result;
+        }
+
+        public async Task<LibMembership> GetMembershipByIdAsync(string membershipId)
+        {
+            var result = await _context.LibMemberships.FirstOrDefaultAsync(x => x.MembershipId.Contains(membershipId));
+
+            if (result == null) throw new ArgumentException(membershipId + " not found");
+
+            return result;
+        }
+
+        public async Task<string> CreateMembershipAsync(LibMembership membership)
+        {
+            _context.LibMemberships.Add(membership);
+            await _context.SaveChangesAsync();
+            return membership.MembershipId;
+        }
+
+        public async Task<string> DisableMembershipAsync(string membershipId)
+        {
+            var result = await _context.LibMemberships.FirstOrDefaultAsync(x => x.MembershipId == membershipId);
+
+            if (result == null) throw new ArgumentException(membershipId + " not found");
+
+            result.Status = 1;
+            _context.LibMemberships.Update(result);
+            await _context.SaveChangesAsync();
+            return membershipId;
+        }
+
+        public async Task<string> EnableMembershipAsync(string membershipId)
+        {
+            var result = await _context.LibMemberships.FirstOrDefaultAsync(x => x.MembershipId == membershipId);
+
+            if (result == null) throw new ArgumentException(membershipId + " not found");
+
+            result.Status = 0;
+            _context.LibMemberships.Update(result);
+            await _context.SaveChangesAsync();
+            return membershipId;
+        }
+
+        public async Task<string> ExtendMembershipAsync(string membershipId)
+        {
+            var result = await _context.LibMemberships.FirstOrDefaultAsync(x => x.MembershipId == membershipId);
+
+            if (result == null) throw new ArgumentException(membershipId + " not found");
+
+            result.ExpiryDate = DateTime.Now.AddYears(1);
+            _context.LibMemberships.Update(result);
+            await _context.SaveChangesAsync();
+            return membershipId;
+        }
+    }
+}
