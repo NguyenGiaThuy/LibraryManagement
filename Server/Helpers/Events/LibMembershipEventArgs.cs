887deba221e364c8ae48d07b7dce68babec01627
thuy <nguyengiathuy1998@gmail.com>
2022-05-30 00:45:31 +0700

    Updated Helpers

--- Server/Helpers/Events/LibMembershipEventArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.Helpers.Events
-{
-    public class LibMembershipEventArgs : EventArgs
-    {
-        public string MembershipId { get; set; } = null!;
-        public string socialId { get; set; } = null!;
-    }
-}
