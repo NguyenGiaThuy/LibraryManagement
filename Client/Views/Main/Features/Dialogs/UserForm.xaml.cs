0bb036938cf3f74462249b56f41befa46f140678
thuy <nguyengiathuy1998@gmail.com>
2022-06-11 23:22:40 +0700

    asgafgdhfg

--- Client/Views/Main/Features/Dialogs/UserForm.xaml.cs
+++ Client/Views/Main/Features/Dialogs/UserForm.xaml.cs
@@ -20,45 +20,6 @@ namespace Client.Views.Main.Features.Dialogs
             InitializeComponent();
         }
 
-        private async Task<bool> UserExistsAsync(string path)
-        {
-<<<<<<< HEAD
-            bool result = false;
-            var response = await App.Client.GetAsync(path);
-            if (response.IsSuccessStatusCode) result = true;
-            return result;
-=======
-            LibUser user = new LibUser();
-
-            user.UserId = UserIdTxt.Text;
-            user.Password = PasswordTxt.Password;
-            user.Name = NameTxt.Text;
-            user.Address = AddressTxt.Text;
-
-            DateTime dob;
-            if(DateTime.TryParse(DateOfBirthComboBox.Text, out dob)) user.Dob = dob;
-            else user.Dob = null;
-
-            user.Mobile = MobileTxt.Text;
-
-            // BUG
-            //int education =
-            //user.Education = int.TryParse(EducationTxt.Text);
-
-
-            //user.Department = int.Parse(DepartmentTxt.Text);
-            //user.Position = int.Parse(PositionTxt.Text);
-            //user.Status = int.Parse(StatusTxt.Text);
-
-            // Update database
-            user = await UpdateUserAsync($"api/libusers/{user.UserId}", user);
-
-            OnUserFormSaved?.Invoke(user);
-
-            Hide();
->>>>>>> parent of 17916c3 (Add CreateForms)
-        }
-
         private async Task<string> CreateUserAsync(string path, LibUser user)
         {
             var response = await App.Client.PostAsJsonAsync(path, user);
@@ -77,53 +38,53 @@ namespace Client.Views.Main.Features.Dialogs
 
         private async void UserFormSaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            bool userExists = await UserExistsAsync($"api/libusers/{UserIdTxt.Text.Trim()}");
-            LibUser user = new LibUser()
-            {
-                UserId = UserIdTxt.Text.Trim(),
-                Password = PasswordTxt.Password.Trim(),
-                Name = NameTxt.Text.Trim(),
-                Address = AddressTxt.Text.Trim(),
-                Mobile = MobileTxt.Text.Trim(),
-                Dob = DateOfBirthComboBox.Text != null ? DateTime.ParseExact(DateOfBirthComboBox.Text, "dd-MM-yyyy", CultureInfo.InvariantCulture) : null,
-                Education = EducationComboBox.SelectedIndex != -1 ? (LibUser.UserEducation)EducationComboBox.SelectedIndex : null,
-                Department = DepartmentComboBox.SelectedIndex != -1 ? (LibUser.UserDepartment)DepartmentComboBox.SelectedIndex : null,
-                Position = PositionComboBox.SelectedIndex != -1 ? (LibUser.UserPosition)PositionComboBox.SelectedIndex : null,
-                //user.ImageUrl = imageUrlTxt.Text
-            };
+            //bool userExists = await UserExistsAsync($"api/libusers/{UserIdTxt.Text.Trim()}");
+            //LibUser user = new LibUser()
+            //{
+            //    UserId = UserIdTxt.Text.Trim(),
+            //    Password = PasswordTxt.Password.Trim(),
+            //    Name = NameTxt.Text.Trim(),
+            //    Address = AddressTxt.Text.Trim(),
+            //    Mobile = MobileTxt.Text.Trim(),
+            //    Dob = DateOfBirthComboBox.Text != null ? DateTime.ParseExact(DateOfBirthComboBox.Text, "dd-MM-yyyy", CultureInfo.InvariantCulture) : null,
+            //    Education = EducationComboBox.SelectedIndex != -1 ? (LibUser.UserEducation)EducationComboBox.SelectedIndex : null,
+            //    Department = DepartmentComboBox.SelectedIndex != -1 ? (LibUser.UserDepartment)DepartmentComboBox.SelectedIndex : null,
+            //    Position = PositionComboBox.SelectedIndex != -1 ? (LibUser.UserPosition)PositionComboBox.SelectedIndex : null,
+            //    //user.ImageUrl = imageUrlTxt.Text
+            //};
 
-            switch (userExists)
-            {
-                case false: // Create user
-                    try
-                    {
-                        await CreateUserAsync($"api/libusers", user);
+            //switch (userExists)
+            //{
+            //    case false: // Create user
+            //        try
+            //        {
+            //            await CreateUserAsync($"api/libusers", user);
 
-                        OnUserFormSaved?.Invoke(user);
+            //            OnUserFormSaved?.Invoke(user);
 
-                        Hide();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
-                    break;
+            //            Hide();
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            MessageBox.Show(ex.Message);
+            //        }
+            //        break;
 
-                case true: // Update user
-                    try
-                    {
-                        await UpdateUserAsync($"api/libusers/{user.UserId}", user);
+            //    case true: // Update user
+            //        try
+            //        {
+            //            await UpdateUserAsync($"api/libusers/{user.UserId}", user);
 
-                        OnUserFormSaved?.Invoke(user);
+            //            OnUserFormSaved?.Invoke(user);
 
-                        Hide();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                    }
-                    break;
-            } 
+            //            Hide();
+            //        }
+            //        catch (Exception ex)
+            //        {
+            //            MessageBox.Show(ex.Message);
+            //        }
+            //        break;
+            //} 
         }
 
         private void UserFormCancelBtn_Click(object sender, RoutedEventArgs e)
