94f6dea76a321830dbd1ee51812c8af55b7f3bc2
Ly Quoc Binh <lyquocbinh02@gmail.com>
2022-06-16 13:25:52 +0700

    Update all roles

--- Client/Views/Main/Users/LibrarianView.xaml.cs
+++ Client/Views/Main/Users/LibrarianView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Client.Views.Main.Features;
+using Client.Views.Main.Features.Dashboard;
+using System;
 using System.Windows;
 
 namespace Client.Views.Main.Users
@@ -8,9 +10,35 @@ namespace Client.Views.Main.Users
     /// </summary>
     public partial class LibrarianView : Window
     {
+        LibrarianDashboardView librarianDashboardView = new LibrarianDashboardView();
+
+        BookView bookView = new BookView();
+        MemberView memberView = new MemberView();
+        CallCardView callCardView = new CallCardView();
+
         public LibrarianView()
         {
+            this.DataContext = this;
             InitializeComponent();
+
+            LibrarianFrame.Content = librarianDashboardView.Content;
+        }
+
+        //LIBRARIAN ====================================================================================
+        private void LibrarianDashboardRBtn_Checked(object sender, RoutedEventArgs e) {
+            LibrarianFrame.Content = librarianDashboardView.Content;
+        }
+
+        private void LibrarianMemberRBtn_Checked(object sender, RoutedEventArgs e) {
+            LibrarianFrame.Content = memberView.Content;
+        }
+
+        private void LibrarianBookRBtn_Checked(object sender, RoutedEventArgs e) {
+            LibrarianFrame.Content = bookView.Content;
+        }
+
+        private void LibrarianCallCardRBtn_Checked(object sender, RoutedEventArgs e) {
+            LibrarianFrame.Content = callCardView.Content;
         }
 
         //Close properly the app when the exit button is pressed
