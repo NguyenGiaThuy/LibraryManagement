3b50ad59b9febfd00aa69657f2abbc10aa45746a
Ly Quoc Binh <lyquocbinh02@gmail.com>
2022-06-16 13:25:52 +0700

    Update all roles

--- Client/Views/Main/Users/StorekeeperView.xaml.cs
+++ Client/Views/Main/Users/StorekeeperView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Client.Views.Main.Features;
+using Client.Views.Main.Features.Dashboard;
+using System;
 using System.Windows;
 
 namespace Client.Views.Main.Users
@@ -8,9 +10,29 @@ namespace Client.Views.Main.Users
     /// </summary>
     public partial class StorekeeperView : Window
     {
-        public StorekeeperView()
-        {
+        StorekeeperDashboardView storekeeperDashboardView = new StorekeeperDashboardView();
+
+        BookView bookView = new BookView();
+        BookAuditCardView bookAuditCardView = new BookAuditCardView();
+
+        public StorekeeperView() {
+            this.DataContext = this;
             InitializeComponent();
+
+            StorekeeperFrame.Content = storekeeperDashboardView.Content;
+        }
+
+        //STOREKEEPER ==================================================================================
+        private void StorekeeperDashboardRBtn_Checked(object sender, RoutedEventArgs e) {
+            StorekeeperFrame.Content = storekeeperDashboardView.Content;
+        }
+
+        private void StorekeeperBookRBtn_Checked(object sender, RoutedEventArgs e) {
+            StorekeeperFrame.Content = bookView.Content;
+        }
+
+        private void StorekeeperBACardRBtn_Checked(object sender, RoutedEventArgs e) {
+            StorekeeperFrame.Content = bookAuditCardView.Content;
         }
 
         //Close properly the app when the exit button is pressed
--- Client/Views/Main/Users/TreasurerView.xaml.cs
+++ Client/Views/Main/Users/TreasurerView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Client.Views.Main.Features;
+using Client.Views.Main.Features.Dashboard;
+using System;
 using System.Windows;
 
 namespace Client.Views.Main.Users
@@ -8,9 +10,30 @@ namespace Client.Views.Main.Users
     /// </summary>
     public partial class TreasurerView : Window
     {
+        TreasurerDashboardView treasurerDashboardView = new TreasurerDashboardView();
+
+        CallCardView callCardView = new CallCardView();
+        FineCardView fineCardView = new FineCardView();
+
         public TreasurerView()
         {
+            this.DataContext = this;
             InitializeComponent();
+
+            TreasurerFrame.Content = treasurerDashboardView.Content;
+        }
+
+        //TREASURER ====================================================================================
+        private void TreasurerDashboardRBtn_Checked(object sender, RoutedEventArgs e) {
+            TreasurerFrame.Content = treasurerDashboardView.Content;
+        }
+
+        private void TreasurerCallCardRBtn_Checked(object sender, RoutedEventArgs e) {
+            TreasurerFrame.Content = callCardView.Content;
+        }
+
+        private void TreasurerFineCardRBtn_Checked(object sender, RoutedEventArgs e) {
+            TreasurerFrame.Content = fineCardView.Content;
         }
 
         //Close properly the app when the exit button is pressed
